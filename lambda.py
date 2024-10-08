6b284237a9748260df892fb4adb9966c3993e425
Sergiy <makov.sergey@gmail.com>
2024-10-08 16:32:21 -0700

    Updated form submition logic

--- lambda.py
+++ lambda.py
@@ -1,6 +1,5 @@
 import json
 import boto3
-import os
 import logging
 import base64
 from urllib.parse import parse_qs
@@ -30,13 +29,17 @@ def lambda_handler(event, context):
         else:
             body = event['body']
             if isinstance(body, str):
-                form_data = json.loads(body)
+                form_data = parse_qs(body)
             else:
                 form_data = body
-        
+
         # Extract single values from the parsed form data
         name = form_data.get('name', ['Guest'])[0]
         user_email = form_data.get('userEmail', ['no-reply@example.com'])[0]
+        phone_number = form_data.get('phoneNumber', ['Not provided'])[0]
+        zipcode = form_data.get('zipcode', ['Not provided'])[0]
+        project_type = form_data.get('projectType', ['Not provided'])[0]
+        property_type = form_data.get('propertyType', ['Not provided'])[0]
         message_content = form_data.get('message', ['No message provided.'])[0]
 
     except Exception as e:
@@ -53,11 +56,21 @@ def lambda_handler(event, context):
         }
     
     # Log the parsed data
-    logger.info(f"Parsed data - Name: {name}, Email: {user_email}, Message: {message_content}")
+    logger.info(f"Parsed data - Name: {name}, Email: {user_email}, Phone: {phone_number}, Zipcode: {zipcode}, Project Type: {project_type}, Property Type: {property_type}, Message: {message_content}")
 
     # Prepare the email content
     subject = 'New Form Submission'
-    body_text = f"You have received a new form submission.\n\nName: {name}\nEmail: {user_email}\nMessage:\n{message_content}"
+    body_text = f"""You have received a new form submission.
+
+Name: {name}
+Email: {user_email}
+Phone Number: {phone_number}
+Zipcode: {zipcode}
+Project Type: {project_type}
+Property Type: {property_type}
+Message:
+{message_content}
+"""
     body_html = f"""
     <html>
     <head></head>
@@ -65,6 +78,10 @@ def lambda_handler(event, context):
       <h1>New Form Submission</h1>
       <p><strong>Name:</strong> {name}</p>
       <p><strong>Email:</strong> {user_email}</p>
+      <p><strong>Phone Number:</strong> {phone_number}</p>
+      <p><strong>Zipcode:</strong> {zipcode}</p>
+      <p><strong>Project Type:</strong> {project_type}</p>
+      <p><strong>Property Type:</strong> {property_type}</p>
       <p><strong>Message:</strong><br>{message_content}</p>
     </body>
     </html>
@@ -88,7 +105,7 @@ def lambda_handler(event, context):
                     'Text': {
                         'Data': body_text,
                         'Charset': 'UTF-8'
-                        },
+                    },
                     'Html': {
                         'Data': body_html,
                         'Charset': 'UTF-8'
